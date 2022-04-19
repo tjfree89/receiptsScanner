d1d0ffea704ac64400c27dfeb140c33ff7b5eb60
Alex Claggett <alex.claggett@gmail.com>
2022-04-19 17:10:12 -0400

    Added scanner activity

---
+++ app/src/main/java/com/example/receiptsscanner/TextRecognitionActivity.kt
@@ -0,0 +1,108 @@
+package com.example.receiptsscanner
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+
+class TextRecognitionActivity : AppCompatActivity() {
+
+    lateinit var recogResult:TextView
+    lateinit var btnChoose:Button
+
+    var intentActivityResultLauncher:ActivityResultLauncher<Intent>?=null
+
+    lateinit var inputImage: InputImage
+    lateinit var textRecognizer:TextRecognizer
+    private val STORAGE_PERMISSION_CODE=135
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_recognition)
+
+        recogResult=findViewById(R.id.recogResult)
+        btnChoose=findViewById(R.id.btnChoose)
+
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+        intentActivityResultLauncher=registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback {
+                //here we handle result
+                val data=it.data
+                val imageUri=data?.data
+
+                if (imageUri != null) {
+                    convertImageToText(imageUri)
+                }
+            }
+        )
+        btnChoose.setOnClickListener {
+            val chooseIntent = Intent()
+            chooseIntent.type="image/*"
+            chooseIntent.action=Intent.ACTION_GET_CONTENT
+            intentActivityResultLauncher?.launch(chooseIntent)
+        }
+    }
+
+    private fun convertImageToText(imageUri: Uri) {
+        try{
+            //prepare the input image
+            inputImage = InputImage.fromFilePath(applicationContext, imageUri)
+
+            //get text from input image
+            val result: Task<Text> = textRecognizer.process(inputImage)
+                .addOnSuccessListener {
+                    recogResult.text = it.text
+                }.addOnFailureListener {
+                    recogResult.text = "Error : ${it.message}"
+                }
+        }catch (e:Exception){
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
+    }
+
+    private fun checkPermission(permission: String, requestCode:Int){
+        if(ContextCompat.checkSelfPermission(this@TextRecognitionActivity, permission)== PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this@TextRecognitionActivity, arrayOf(permission), requestCode)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this@TextRecognitionActivity, "Storage Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@TextRecognitionActivity, "Storage Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+}
